9239ea1a9a4176e544c898b7da5fe0738df264b4
AMaranhao <alexandreqmj@gmail.com>
2023-10-07 01:06:25 -0300

    Primeiro Commit

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+import { z } from 'zod'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.get('/denuncia', async () => {
+    const denuncia = await prisma.denuncia.findMany()
+
+    return{ denuncia }
+})
+
+app.post('/denuncia', async (request, reply) => {
+    const createDenunciaSchema = z.object({
+        latitude: z.number(),
+        longitude: z.number(),
+        imagem: z.string(),
+        textoDenuncia: z.string(),
+        data: z.date(),
+    })
+
+    const{ latitude, longitude, imagem, textoDenuncia, data} = createDenunciaSchema.parse(request.body)
+
+    await prisma.denuncia.create({
+        data:{
+            latitude, 
+            longitude, 
+            imagem, 
+            textoDenuncia, 
+            data
+        }
+    })
+    return reply.status(201).send()
+})
+
+
+app.listen({ 
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT):3333,
+    }).then(() => {
+    console.log('HTTP Server running')
+})
